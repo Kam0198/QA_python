8ce6a72a60ccfa9db3742e6de4c3f8957996c706
Kamilla Gabdrakhmanova <kam0198@yandex.ru>
2023-12-14 23:33:47 +0300

    aqa_python_4

--- tests.py
+++ tests.py
@@ -1,24 +1,77 @@
 from main import BooksCollector
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book("Принцесса")
+        collector.set_book_genre("Принцесса", "Мультфильмы")
+        books_for_children = collector.get_books_for_children()
+        assert "Принцесса" in books_for_children
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("12 стульев")
+        collector.set_book_genre("12 стульев", "Комедии")
+        assert collector.get_book_genre("12 стульев") == "Комедии"
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Сияние")
+        collector.set_book_genre("Сияние", "Ужасы")
+        books_with_genre = collector.get_books_with_specific_genre("Ужасы")
+        assert "Сияние" in books_with_genre
+
+    def test_get_book_genre_nonexistent_book(self):
+        collector = BooksCollector()
+        genre = collector.get_book_genre("NonexistentBook")
+        assert genre is None
+
+    def test_add_book_without_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Голодные игры")
+        assert collector.get_book_genre("Голодные игры") == ""
+
+    def test_add_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Источник")
+        collector.add_book_in_favorites("Источник")
+        favorites = collector.get_list_of_favorites_books()
+        assert "Источник" in favorites
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Зелёный свет")
+        collector.add_book_in_favorites("Зелёный свет")
+
+        collector.delete_book_from_favorites("Зелёный свет")
+        favorites_after_delete = collector.get_list_of_favorites_books()
+        assert "Зелёный свет" not in favorites_after_delete
+
+    def test_books_with_age_rating_not_for_children_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Мгла")
+        collector.set_book_genre("Мгла", "Ужасы")
+        books_for_children = collector.get_books_for_children()
+        assert "Мгла" not in books_for_children
+
+    def test_add_same_book_once(self):
+        collector = BooksCollector()
+        collector.add_new_book("Одноэтажная Америка")
+        try:
+            collector.add_new_book("Одноэтажная Америка")
+        except ValueError as e:
+            assert str(e) == 'Книга с таким названием уже существует'
+
+    def test_get_book_genre(self, sample_collector):
+        genre_book1 = sample_collector.get_book_genre("Звёздные войны")
+        genre_book2 = sample_collector.get_book_genre("Смерть на Ниле")
+        assert genre_book1 == "Фантастика"
+        assert genre_book2 == "Ужасы"
